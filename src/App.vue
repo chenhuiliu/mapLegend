9b29300a0458961983d23a564a2256584d0050bb
chenhui <1119551708@qq.com>
2020-05-12 17:45:08 +0800

    lengend组件

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <filterMenu :data="list" v-on:getSeletedData='getData'/>
+  </div>
+</template>
+
+<script>
+import filterMenu from "./components/filterMenu/index";
+
+export default {
+  name: "App",
+  components: {
+    filterMenu
+  },
+  data: function() {
+    return {
+      list: [
+        {
+          name: "奥迪",
+          url: "http://qa5vj5o3r.bkt.clouddn.com/%E5%A5%A5%E8%BF%AA%20%284%29.png",
+          isSelected: false
+        },
+        {
+          name: "一汽大众",
+          url: "http://qa5vj5o3r.bkt.clouddn.com/%E5%A4%A7%E4%BC%97.png",
+          isSelected: false
+        },
+        {
+          name: "充电桩",
+          url: "http://qa5vj5o3r.bkt.clouddn.com/%E5%A5%A5%E8%BF%AA%20%284%29.png",
+          isSelected: false
+        },
+        {
+          name: "特斯拉",
+          url: "http://qa5vj5o3r.bkt.clouddn.com/%E5%A5%A5%E8%BF%AA%20%284%29.png",
+          isSelected: false
+        },
+        {
+          name: "奥迪2",
+          url: "http://qa5vj5o3r.bkt.clouddn.com/%E5%A5%A5%E8%BF%AA%20%284%29.png",
+          isSelected: false
+        }
+      ]
+    }
+  },
+  methods: {
+    getData: function(a) {
+      console.log(11111111111,a);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/filterMenu/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div id="container" class="map-box"></div>
+    <div class="lengend-box">
+      <ul>
+        <li
+          v-for="(item, index) in data"
+          :key="item.name"
+          @click="selectHandler(index, item)"
+          :class="{'active': item.isSelected }"
+        >
+          <img :src="item.url" alt />
+          <span>{{item.name}}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Lengend",
+  props: {
+    data: Array
+  },
+  mounted: function() {
+    console.log(this.data, "====data====");
+  },
+  methods: {
+    selectHandler: function(index, item) {
+      item.isSelected = !item.isSelected;
+      this.$emit('getSeletedData', item);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map-box {
+  height: 100vh;
+  background: rebeccapurple;
+}
+
+.lengend-box {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.lengend-box ul {
+  display: flex;
+  justify-content: center;
+}
+
+.lengend-box li {
+  display: flex;
+  align-items: center;
+  width: 220px;
+  list-style-type: none;
+  margin-right: 10px;
+  user-select: none;
+  padding: 16px;
+  background: #fff;
+  color: #000;
+  font-size: 14px;
+}
+
+li img {
+  width: 20px;
+  height: 20px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-right: 15px;
+}
+
+.lengend-box .active {
+  border-bottom: 3px solid red;
+}
+</style>
